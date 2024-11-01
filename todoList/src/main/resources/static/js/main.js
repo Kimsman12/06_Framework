526686e3007f2fe15e7daa763a624348e15ebdc5
Kimsman12 <kkm01030103@gmail.com>
2024-11-01 17:45:56 +0900

    js추가

--- todoList/src/main/resources/static/js/main.js
+++ todoList/src/main/resources/static/js/main.js
@@ -1,29 +1,25 @@
-const submitBtn = document.querySelector(".btn");
 const todoTitle = document.querySelector("#todoTitle");
 const addForm = document.querySelector("#updateForm");
 const todoContent = document.querySelector("#todoContent");
+const addBtn = document.querySelector("#addBtn");
 
-addForm.addEventListener("submit", e => {
+addBtn.addEventListener("click", e => {
 
   const inputTitle = todoTitle.value.trim();
   const inputContent = todoContent.value.trim();
 
-  if(inputTitle.length===0){
+  if(inputTitle.length === 0) {
     e.preventDefault();
     alert("제목을 입력해 주세요");
     todoTitle.focus();
-
-    return;
-  } 
-  if (inputContent.length===0){
-    e.preventDefault();
-    alert("내용을 입력해 주세요");
-    todoContent.focus();
-    
     return;
   }
 
-});
+  if(inputContent.length === 0) {
+    e.preventDefault();
+    alert("내용을 작성해 주세요");
+    todoContent.focus();
+    return;
+  }
 
-
-
+})
